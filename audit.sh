59a4c47bff7a993c70cf04f19b764d34d436fbee
Leigh London <leigh.00.london@gmail.com>
2016-09-05 21:26:27 +1000

    SAVEPOINT

--- audit.sh
+++ audit.sh
@@ -8,7 +8,7 @@
 SHA256SUM="sha256sum"
 TIMEOUT="timeout"
 # Set the timeout duration.
-TIMEOUT_DURATION=240
+TIMEOUT_DURATION=40
 # Brute force filename.
 BRUTE_FORCE_FILENAME="eletric-brute-force.txt"
 # Default dictionary to use.
@@ -58,28 +58,26 @@ brute_force() {
         # Calculate the attempted hash.
         try=$(sha_hash "$PLAINTEXT")
         # If it matches, echo and return.
-        if "$try" == "$input_hash"; then
+        if [ "$try" == "$input_hash" ]; then
             echo "$PLAINTEXT"
             return
         fi
-    done < $BRUTE_FORCE_FILENAME
+    done < "$BRUTE_FORCE_FILENAME"
 }
 
 # Export the function to make it callable with a timeout.
-export -f sha_hash
+export -f sha_hash brute_force
+export BRUTE_FORCE_FILENAME
 read_input_file
-
-# Try calculating the hash.
-try=$(sha_hash "a")
-
-# Iterate the dictionary and display it.
-for i in "${!users[@]}"; do
-    # Echo and enable tabs (-e)
-    echo -e "user:" "$i" "\tpassword:" "${users[$i]}"
-    $TIMEOUT $TIMEOUT_DURATION bash -c "sha_hash ${users[$i]}"
-done
 
 # Only generate the brute force file if it doesn't already exist.
 if [ ! -f $BRUTE_FORCE_FILENAME ]; then
     gen_brute_force
 fi
+
+# Iterate the dictionary and display it.
+for i in "${!users[@]}"; do
+    # Echo and enable tabs (-e)
+    att=$($TIMEOUT $TIMEOUT_DURATION bash -c "brute_force ${users[$i]}")
+    echo $att
+done
